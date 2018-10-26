43459c53af181bd163437f1526a70aa23a0a3056
jeremie80 <lbjr.webmaster@gmail.com>
2018-10-26 11:50:41 +0400

    reccup des données + debug de grunt serve

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -8,10 +8,20 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+  
+  $scope.getArticles = function(){
+    $http.get('http://localhost:3000/api/Articles').then(function(response) {
+       return $scope.articles = response.data;
+      console.log($scope.articles);
+  });
+  };
+
+
+    
   });
